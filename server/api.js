196ae76b7cdffe0d79a228461b618230da620330
Zsolt Sz. Sztupák <mail@sztupy.hu>
2024-01-26 23:58:09 +0000

    Implement Level 120 - Backend
    Loads up the videos from the database and then returns them as an array. We explicitly map the values to make sure we don't expose any unattended columns

--- server/api.js
+++ server/api.js
@@ -2,9 +2,17 @@ import { Router } from "express";
 import db from "./db.js";
 const router = Router();
 
+function videoToJson(video) {
+	return {
+		id: video.id,
+		url: video.url,
+		title: video.title,
+	};
+}
+
 router.get("/videos", async (_, res) => {
-	const result = await db.query("SELECT 'ok' ok");
-	res.json(result.rows[0]);
+	const result = await db.query("SELECT * FROM videos");
+	res.status(200).json(result.rows.map(videoToJson));
 });
 
 export default router;
