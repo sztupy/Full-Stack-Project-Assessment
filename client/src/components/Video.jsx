53d07c94b06aa14b3a2bcd0d1a2be785aa357167
Zsolt Sz. Sztupák <mail@sztupy.hu>
2024-01-27 00:01:34 +0000

    Implement Level 200 - Embedded videos and better page design
    Adds support to show videos in iframes based on the requirements. Also updates the entire page to show the videos in a grid. The grid is responsive dependent on the screen width.

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,9 +1,30 @@
+// source: https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
+function youtubeLinkParser(url) {
+	let regExp =
+		/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+	let match = url.match(regExp);
+	return match && match[7].length == 11 ? match[7] : false;
+}
+
 export default function Video({ video }) {
+	const youtubeId = youtubeLinkParser(video.url);
+
 	return (
 		<li className="video">
-			<a href={video.url} tabIndex={0}>
-				{video.title}
-			</a>
+			<h2>
+				<a href={video.url} tabIndex={0}>
+					{video.title}
+				</a>
+			</h2>
+			{youtubeId && (
+				<iframe
+					src={`https://www.youtube.com/embed/${youtubeId}`}
+					title={video.title}
+					frameBorder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowFullScreen
+				></iframe>
+			)}
 		</li>
 	);
 }
