9a8ef80f3d03c0f7ac28665699829e42ec5d36b6
Zsolt Sz. Sztupák <mail@sztupy.hu>
2024-01-28 01:55:29 +0000

    Vercel setup test

--- server/api/index.mjs
+++ server/api/index.mjs
@@ -1,10 +1,3 @@
-import express from "express";
-import serverless from "serverless-http";
 import apiRouter from "../api.js";
 
-const app = express();
-
-app.use(express.json());
-app.use("/api/", apiRouter);
-
-export const handler = serverless(app);
+export default apiRouter;
